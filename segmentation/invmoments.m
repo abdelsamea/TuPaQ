33e11aa2403f4cddc70eba434b4d90dca1c3d750
abdelsamea <44769232+abdelsamea@users.noreply.github.com>
2018-11-19 08:22:55 +0000

    Update invmoments.m

--- segmentation/invmoments.m
+++ segmentation/invmoments.m
@@ -1,15 +1,5 @@
 function phi = invmoments(F)
-%INVMOMENTS Compute invariant moments of image.
-%   PHI = INVMOMENTS(F) computes the moment invariants of the image
-%   F. PHI is a seven-element row vector containing the moment
-%   invariants as defined in equations (11.3-17) through (11.3-23) of
-%   Gonzalez and Woods, Digital Image Processing, 2nd Ed.
-%
-%   F must be a 2-D, real, nonsparse, numeric or logical matrix.
 
-%   Copyright 2002-2004 R. C. Gonzalez, R. E. Woods, & S. L. Eddins
-%   Digital Image Processing Using MATLAB, Prentice-Hall, 2004
-%   $Revision: 1.5 $  $Date: 2003/11/21 14:39:19 $
 
 if (ndims(F) ~= 2) | issparse(F) | ~isreal(F) | ~(isnumeric(F) | ...
                                                     islogical(F))
